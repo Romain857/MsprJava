476c27d7be608903ca2182c7e81049db8caf37e2
Romain857 <romainmart.85@gmail.com>
2022-01-26 18:14:18 +0100

    compare agents/ficheAgent

--- src/main/java/listeAgents.java
+++ src/main/java/listeAgents.java
@@ -1,8 +1,11 @@
 import java.io.*;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
 public class listeAgents {
+    private static Object MessageBox;
+
     public static void main(String[] args) throws Exception{
 
         System.out.println("*********** Liste de fichiers **********");
@@ -12,19 +15,21 @@ public class listeAgents {
         for(File item : liste){
             if(item.isFile())
             {
-                System.out.format("%s%n", item.getName());
-                listFichiers.add(item.getName());
+                //System.out.format("%s%n", item.getName());
+                String fichierWithOutExt = item.getName().replaceFirst("[.][^.]+$", "");
+                listFichiers.add(fichierWithOutExt);
             }
         }
         for(int i = 0;i < listFichiers.size();i++){
             System.out.println( i + 1 + " : " + listFichiers.get(i));
         }
+
         System.out.println("*********** Accueil **********");
         String file = "C:\\Users\\romai\\Desktop\\TxtGoSecuri\\ListeAgents\\staff.json";
+        List<String> list = new ArrayList<String>();
         try(BufferedReader br = new BufferedReader(new FileReader(file)))
         {
             String line;
-            List<String> list = new ArrayList<String>();
             while ((line = br.readLine()) != null) {
                 //System.out.println(line);
                 list.add(line);
@@ -36,6 +41,15 @@ public class listeAgents {
         catch (IOException e) {
             System.out.println("Rien dans le txt");
             e.printStackTrace();
+        }
+
+        if (listFichiers.equals(list)){
+            System.out.println("Tous les agents ont inscris leur fiche agent");
+        }else{
+            System.out.println("Des agents n'ont pas inscris leur liste agents");
+            List<String> listCompare = new ArrayList<String>(list);
+            listCompare.retainAll(listFichiers);
+            System.out.println("Seulement " + listCompare + " ont inscris leur fiche agent");
         }
     }
 }
